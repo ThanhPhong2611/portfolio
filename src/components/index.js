ef450a1835e4ba7252543525f61fcab86ec1e403
Ver Sama <hothanhphong2611@gmail.com>
2024-04-15 20:16:14 +0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Loader from "./Loader";
+import HomeInfo from "./HomeInfo";
+import Alert from "./Alert";
+import CTA from "./CTA";
+export {
+    Loader,
+    Navbar,
+    Footer,
+    HomeInfo,
+    Alert,
+    CTA,
+}
